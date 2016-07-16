799f20b69329aac2ea9095340a0853cd42d18ab9
shuhantao <619508545@qq.com>
2016-07-17 00:11:38 +0800

    Rejected commit: Default

--- app/src/main/java/com/logistical/logistical/MainActivity.java
+++ app/src/main/java/com/logistical/logistical/MainActivity.java
@@ -9,20 +9,20 @@ import android.widget.Toast;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.HashMap;
-import express.*;
+
 
 
 public class MainActivity extends AppCompatActivity{
     public static final String ATTRIBUTE_NAMES[][] = { { "发站", "到站" }, { "发货人", "发货人电话" }, { "收货人", "收货人电话" },
             { "付款方式", "品类", "小类", "件数", "单价", "已付", "提付", "月结" }, { "代收款", "返款方", "从", "到", "保价费", "接货费", "送运费" },
-            { "总件数", "总运费" } };
+            {"总件数", "总运费"  }};
     public static final String edit[] = { "Fname", "Ftel" ,
             "Tname", "Ttel" ,
              "number", "uniprice", "HavePay", "ToPay", "MonthPay" ,
-            "daishou", "fankuan", "baojia", "jiehuo", "songyun" , { "totnumber", "totpay"  };
+            "daishou", "fankuan", "baojia", "jiehuo", "songyun" ,  "totnumber", "totpay"  };
     public static final String list[] ={
         "payWay", "category1", "category2","Fstation", "Tstation","Fbaojia", "Tbaojia",
-    }
+    };
     HashMap<String,EditText> mse = new HashMap<String, EditText>();
     HashMap<String,Spinner> mss = new HashMap<String, Spinner>();
     //HashMap<String,>
